0f2366477aa29039ba757c5ed4c1de762c6dc3cf
Kennet Mathisen <kennetam02@gmail.com>
2022-01-04 15:32:34 +0100

    in the prosses of rewriteing it

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,47 @@
+use std::io;
+use std::io::prelude::*;
+use std::env;
+use read_input::prelude::*;
+
 fn main() {
-    use std::env;
-    let args: Vec<String> = env::args().collect();
     
-        match args.len() {
-            1 => {
-                use read_input::prelude::*;
-                let amount = input::<u32>().msg("Enter amount of items\n>>> ").get();
-                let stack_size = input::<u32>().msg("Enter stack size\n(Default is 64)\n>>> ").default(64).get();
-                let [stack, items] = total_to_stack(stack_size, amount);
-                println!("{} stacks and {} items",stack,items);
-                pause()
-            }
-            2 => {
-                let amount = args.get(1).unwrap().parse::<u32>().unwrap();
-                let [stack, items] = total_to_stack(64, amount);
-                println!("{} stacks and {} items",stack,items);
-            }
-            3 => {
-                let amount = args.get(1).unwrap().parse::<u32>().unwrap();
-                let stack_size = args.get(2).unwrap().parse::<u32>().unwrap();
-                let [stack, items] = total_to_stack(stack_size, amount);
-                println!("{} stacks and {} items",stack,items);
-                
-            }
-            _ => ()
+    let args: Vec<String> = env::args().collect();
+    let args_len = args.len();
+    if args_len > 1 {
+        
+        cli_calc()
+    } else {
+        terminal_calc()
+    }
+}
+
+fn cli_calc(args:(Option<u32>,Option<u32>)) -> (){
+    match args {
+        (Some(amount), None) => {
+            let [stack, items] = total_to_stack(64, amount);
+            println!("{} stacks and {} items",stack,items);
         }
-       
+        (Some(amount), Some(stack_size)) => {
+            let [stack, items] = total_to_stack(stack_size, amount);
+            println!("{} stacks and {} items",stack,items);
+        }
+        (None, _) => terminal_calc()
+    }
+}
+
+fn terminal_calc()->() {
+    let amount = input::<u32>().msg("Enter amount of items\n").get();
+    let stack_size = input::<u32>().msg("Enter stack size\n(Default is 64)\n").default(64).get();
+    let [stack, items] = total_to_stack(stack_size, amount);
+    println!("{} stacks and {} items",stack,items);
+    pause()
 }
 
 fn total_to_stack(stack_size: u32, total: u32) -> [u32; 2] {
     [total / stack_size, total % stack_size]
 }
 
-use std::io;
-use std::io::prelude::*;
-
-pub fn pause() {
+fn pause() {
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
     write!(stdout, "Press any key to continue...").unwrap();
