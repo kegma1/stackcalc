5e500856833cdf1794e61b0f4976f559e25e5c45
Kennet Mathisen <kennetam02@gmail.com>
2022-01-04 16:45:42 +0100

    redid the logic in prep for error handeling

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,42 @@
+use read_input::prelude::*;
+use std::env;
 use std::io;
 use std::io::prelude::*;
-use std::env;
-use read_input::prelude::*;
 
 fn main() {
-    
     let args: Vec<String> = env::args().collect();
-    let args_len = args.len();
-    if args_len > 1 {
-        
-        cli_calc()
-    } else {
-        terminal_calc()
-    }
-}
 
-fn cli_calc(args:(Option<u32>,Option<u32>)) -> (){
-    match args {
+    let arg_1 = match args.get(1) {
+        Some(v) => Some(v.parse::<u32>().unwrap()),
+        None => None,
+    };
+
+    let arg_2 = match args.get(2) {
+        Some(v) => Some(v.parse::<u32>().unwrap()),
+        None => None,
+    };
+
+    match (arg_1, arg_2) {
         (Some(amount), None) => {
             let [stack, items] = total_to_stack(64, amount);
-            println!("{} stacks and {} items",stack,items);
+            println!("{} stacks and {} items", stack, items);
         }
         (Some(amount), Some(stack_size)) => {
+            println!("lol");
             let [stack, items] = total_to_stack(stack_size, amount);
-            println!("{} stacks and {} items",stack,items);
+            println!("{} stacks and {} items", stack, items);
         }
-        (None, _) => terminal_calc()
+        (None, _) => {
+            let amount = input::<u32>().msg("Enter amount of items\n").get();
+            let stack_size = input::<u32>()
+                .msg("Enter stack size\n(Default is 64)\n")
+                .default(64)
+                .get();
+            let [stack, items] = total_to_stack(stack_size, amount);
+            println!("{} stacks and {} items", stack, items);
+            pause()
+        }
     }
-}
-
-fn terminal_calc()->() {
-    let amount = input::<u32>().msg("Enter amount of items\n").get();
-    let stack_size = input::<u32>().msg("Enter stack size\n(Default is 64)\n").default(64).get();
-    let [stack, items] = total_to_stack(stack_size, amount);
-    println!("{} stacks and {} items",stack,items);
-    pause()
 }
 
 fn total_to_stack(stack_size: u32, total: u32) -> [u32; 2] {
